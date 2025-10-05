75891ca48f087c3d3af57901e8816df541276be0
park-jinh <148922524+park-jinh@users.noreply.github.com>
2025-10-06 00:38:36 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const isCustomDomain = process.env.CUSTOM_DOMAIN === '1'
+
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/yahtzee-front/' : '/',
+  base: isCustomDomain ? '/' : '/yahtzee-front/',
 })
